6453533381fd07051c79c2ee1abab38adc3f3919
Almango <hh1830314643@163.com>
2025-11-19 15:49:30 +0800

    2

---
+++ src/config.ts
@@ -0,0 +1,111 @@
+import type {
+	ExpressiveCodeConfig,
+	LicenseConfig,
+	NavBarConfig,
+	ProfileConfig,
+	SiteConfig,
+} from "./types/config";
+import { LinkPreset } from "./types/config";
+
+export const siteConfig: SiteConfig = {
+	title: "Almango",
+	subtitle: "天真永不消逝，浪漫至死不渝",
+	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
+	themeColor: {
+		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+		fixed: false, // Hide the theme color picker for visitors
+	},
+	banner: {
+		enable: false,
+		src: "assets/images/demo-banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
+		credit: {
+			enable: false, // Display the credit text of the banner image
+			text: "", // Credit text to be displayed
+			url: "", // (Optional) URL link to the original artwork or artist's page
+		},
+	},
+	toc: {
+		enable: true, // Display the table of contents on the right side of the post
+		depth: 2, // Maximum heading depth to show in the table, from 1 to 3
+	},
+	favicon: [
+		// Leave this array empty to use the default favicon
+		{
+			src: "/img/favicon.png", // Path of the favicon, relative to the /public directory
+			theme: "light", // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
+			sizes: "32x32", // (Optional) Size of the favicon, set only if you have favicons of different sizes
+		},
+	],
+};
+
+export const navBarConfig: NavBarConfig = {
+	links: [
+		LinkPreset.Home,
+		LinkPreset.Archive,
+		{
+			name: "友链",
+			url: "/#/",
+			external: false,
+		},
+		{
+			name: "动态",
+			url: "/#/",
+			external: false,
+		},
+		{
+			name: "朋友圈",
+			url: "/#/",
+			external: false,
+		},
+		LinkPreset.About,
+	],
+};
+
+export const profileConfig: ProfileConfig = {
+	avatar: "assets/images/favicon.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	name: "Almango",
+	bio: "天真永不消逝，浪漫至死不渝",
+	links: [
+		{
+			name: "E-mail",
+			icon: "fa6-brands:qq", // Visit https://icones.js.org/ for icon codes
+			// You will need to install the corresponding icon set if it's not already included
+			// `pnpm add @iconify-json/<icon-set-name>`
+			url: "#",
+		},
+		{
+			name: "BiliBili",
+			icon: "fa6-brands:bilibili",
+			url: "https://space.bilibili.com/1278951656?spm_id_from=333.1007.0.0",
+		},
+		{
+			name: "GitHub",
+			icon: "fa6-brands:github",
+			url: "https://github.com/saicaca/fuwari",
+		},
+	],
+};
+
+export const licenseConfig: LicenseConfig = {
+	enable: true,
+	name: "CC BY-NC-SA 4.0",
+	url: "https://creativecommons.org/licenses/by-nc-sa/4.0/",
+};
+
+export const expressiveCodeConfig: ExpressiveCodeConfig = {
+	// Note: Some styles (such as background color) are being overridden, see the astro.config.mjs file.
+	// Please select a dark theme, as this blog theme currently only supports dark background color
+	theme: "github-dark",
+};
+
+export const commentConfig = {
+	enable: true,
+	twikoo: {
+		envId: "https://www.almango.fun/",
+		region: "ap-guangzhou",
+		lang: "zh-CN",
+		avatar: "mp",
+		requiredFields: ["nick", "mail"],
+	},
+};
---
+++ src/constants/icon.ts
@@ -0,0 +1,45 @@
+import type { Favicon } from "@/types/config.ts";
+
+export const defaultFavicons: Favicon[] = [
+	{
+		src: "/img/favicon.png",
+		theme: "light",
+		sizes: "32x32",
+	},
+	{
+		src: "/img/favicon.png",
+		theme: "dark",
+		sizes: "32x32",
+	},
+	{
+		src: "/img/favicon.png",
+		theme: "light",
+		sizes: "128x128",
+	},
+	{
+		src: "/img/favicon.png",
+		theme: "light",
+		sizes: "180x180",
+	},
+	{
+		src: "/img/favicon.png",
+		theme: "light",
+		sizes: "192x192",
+	},
+
+	{
+		src: "/img/favicon.png",
+		theme: "dark",
+		sizes: "128x128",
+	},
+	{
+		src: "/img/favicon.png",
+		theme: "dark",
+		sizes: "180x180",
+	},
+	{
+		src: "/img/favicon.png",
+		theme: "dark",
+		sizes: "192x192",
+	},
+];
